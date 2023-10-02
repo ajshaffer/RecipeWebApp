82f9ece88840d1576c0d26745179cc1af784f9ab
Austin Shaffer <ajshaffer@coastal.edu>
2023-10-02 11:33:20 -0400

    Added more files

---
+++ RecipeApp/footer.php
@@ -0,0 +1,13 @@
+
+
+
+
+<footer>
+    <?php
+    //Current Date/Time using the date function,  not the DateTime class.
+    echo date("Y"); //This is using the date function
+    ?>
+</footer>
+</body>
+</html>
+
