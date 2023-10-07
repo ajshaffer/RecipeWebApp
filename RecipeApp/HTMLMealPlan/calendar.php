5b321447674d590467aaaa07b56032d4a2d7a1c9
ashtyncarpenter <99212646+ashtyncarpenter@users.noreply.github.com>
2023-10-07 15:04:18 -0400

    making an alternate calendar

---
+++ RecipeApp/HTMLMealPlan/calendar.php
@@ -0,0 +1,24 @@
+<?php
+//$pageName = "Home";
+
+
+//require_once "header.php";
+//require_once "connect.php";
+//require_once "functions.php";
+
+?>
+    <!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
+<?php
+    //require_once "footer.php"
+?>
