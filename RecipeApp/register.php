516146d49d146cea6b796813039ca91299eb20e3
Austin Shaffer <ajshaffer@coastal.edu>
2023-10-09 18:39:56 -0400

    login and registration UI, error checking, and submission features

--- RecipeApp/register.php
+++ RecipeApp/register.php
@@ -71,9 +71,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
   
 
 
-
-
-
 if($showForm == 1){
 ?>
 
@@ -90,49 +87,58 @@ if($showForm == 1){
 <body>
 
 
-    <div class="container">
-    <h2>Registration</h2>
+<div class="container">
+    <div class = "left-side left-side-registration">
+        <!-- Image placed here with CSS -->
+    </div>
 
-        <div class="form-container">
+        <div class="right-side">
+            <div class = "right-side-text">
+                <h2>Registration</h2>
+            </div>
 
-            <!-- Form signup -->
-            <form id="signup-form" class="form" method="POST" action="index.php">
+            <div class = "signup-form">
+                <!-- Form signup -->
+                <form id="signup-form" class="form" method="POST" action="index.php">
 
-                <h2>Sign Up</h2>
+                    <h2>Sign Up</h2>
 
-                <div class="form-group">
-                    <label for="fname">First Name:</label>
-                    <input type="text" id="fname" name="fname" required placeholder="Enter your first name:" maxlength="30" value="<?php if(isset($fname)){ echo htmlspecialchars($fname);}?>">
-                    <span class="error"> <?php echo $err_fname;?></span><br>
-                </div>
+                    <div class="form-group">
+                        <label for="fname">First Name:</label>
+                        <input type="text" id="fname" name="fname" required placeholder="Enter your first name:" maxlength="30" value="<?php if(isset($fname)){ echo htmlspecialchars($fname);}?>">
+                        <span class="error"> <?php echo $err_fname;?></span><br>
+                    </div>
 
-                <div class="form-group">
-                    <label for="lname">Last Name:</label>
-                    <input type="text" id="lname" name="lname" required placeholder="Enter your last name:" maxlength="50" value="<?php if(isset($lname)){ echo htmlspecialchars($lname);}?>">
-                    <span class="error"> <?php echo $err_lname;?></span><br><br>
-                </div>
+                    <div class="form-group">
+                        <label for="lname">Last Name:</label>
+                        <input type="text" id="lname" name="lname" required placeholder="Enter your last name:" maxlength="50" value="<?php if(isset($lname)){ echo htmlspecialchars($lname);}?>">
+                        <span class="error"> <?php echo $err_lname;?></span><br><br>
+                    </div>
 
-                <div class="form-group">
-                    <label for="new-email">Email:</label>
-                    <input type="email" id="new-email" name="new-email" required placeholder="Enter your email:" value="<?php if(isset($email)){ echo htmlspecialchars($email);}?>">
-                    <span class="error"> <?php echo $err_email;?></span><br><br>
-                </div>
+                    <div class="form-group">
+                        <label for="new-email">Email:</label>
+                        <input type="email" id="new-email" name="new-email" required placeholder="Enter your email:" value="<?php if(isset($email)){ echo htmlspecialchars($email);}?>">
+                        <span class="error"> <?php echo $err_email;?></span><br><br>
+                    </div>
 
-                <div class="form-group">
-                    <label for="pwd">Password:</label>
-                    <input type="password" id="pwd" name="pwd" required placeholder="Enter a password:">
-                    <span class="error"> <?php echo $err_pwd;?></span><br>
-                </div>
+                    <div class="form-group">
+                        <label for="pwd">Password:</label>
+                        <input type="password" id="pwd" name="pwd" required placeholder="Enter a password:">
+                        <span class="error"> <?php echo $err_pwd;?></span><br>
+                    </div>
 
-                <button type="submit" name = "signup">Sign Up</button>
-                
-                <div class = "login-link">
-                    <a href="index.php">Login</a>
-                </div>
+                    <button type="submit" name = "signup">Sign Up</button>
+                    
+                    <div class = "login-link">
+                        <p>Already have an account? <a href="index.php">Login</a>
+                    </div>
 
-            </form>
+                </form>
+            </div>
         </div>
     </div>
+</div>  
+
 </body>
 </html>
 
