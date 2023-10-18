e98db6ed950154c69d4eb5e53a3769c0421b8867
ashtyncarpenter <99212646+ashtyncarpenter@users.noreply.github.com>
2023-10-18 18:13:54 -0400

    worked on user interface for meal planner

---
+++ RecipeApp/mealplanner.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<style>
+table, th, td {
+  border:1px solid black;
+}
+</style>
+<body>
+
+<h2>Meal Planner</h2>
+
+<table style="width:100%">
+  <tr>
+    <th>Sunday</th>
+    <th>Monday</th>
+    <th>Tuesday</th>
+    <th>Wednesday</th>
+    <th>Thursday</th>
+    <th>Friday</th>
+    <th>Saturday</th>
+
+  </tr>
+  <tr>
+    <td>Example</td>
+    <td>Example</td>
+    <td>Example</td>
+    <td>Example</td>
+    <td>Example</td>
+    <td>Example</td>
+    <td>Example</td>
+  </tr>
+ 
+</table>
+
+<p>To understand the example better, we have added borders to the table.</p>
+
+</body>
+</html>
